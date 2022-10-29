84b1aae64655ac935eeea8547409ae1cba72a372
theGeekylad <rahul.pillai03@gmail.com>
2022-10-29 14:53:17 +0530

    Re-organized workflow

---
+++ nav/nav-standard.php
@@ -0,0 +1,16 @@
+<?php
+    echo '
+    <nav>
+        <div class="nav-wrapper blue darken-3">
+        <a href="#" class="brand-logo pl-1 hide-on-med-and-down">The Incredible Insider</a>
+        <ul id="nav-mobile" class="right d-flex justify-center">
+            <!-- hide-on-med-and-down -->
+            <li><a href="../../index.html">Home</a></li>
+            <li><a href="../about/about.php">About</a></li>
+            <li><a href="../news/news.html">News</a></li>
+            <li><a href="../contacts/contacts.php">Contacts</a></li>
+        </ul>
+        </div>
+    </nav>
+    ';
+?>
--- pages/admin/admin.php
+++ pages/admin/admin.php
@@ -16,18 +16,7 @@
 </head>
 
 <body>
-  <nav>
-    <div class="nav-wrapper blue darken-3">
-      <a href="#" class="brand-logo pl-1 hide-on-med-and-down">The Incredible Insider</a>
-      <ul id="nav-mobile" class="right d-flex justify-center">
-        <!-- hide-on-med-and-down -->
-        <li><a href="../../index.html">Home</a></li>
-        <li><a href="../about/about.html">About</a></li>
-        <li><a href="../news/news.html">News</a></li>
-        <li><a href="../contacts/contacts.php">Contacts</a></li>
-      </ul>
-    </div>
-  </nav>
+  <?php include '../../nav/nav-standard.php';?>
 
   <div class="container">
     <h4 class="my-title text-center">Admin Corner</h4>
